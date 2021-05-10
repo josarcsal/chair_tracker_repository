977956319f9004aa50091dd83e3c8c6ab6b15eea
josgarmar28 <martagonyt@gmail.com>
2021-05-10 22:47:27 +0200

    Envio de datos por body

--- chair_tracker/src/main/java/es/us/lsi/dad/HttpServerVerticle.java
+++ chair_tracker/src/main/java/es/us/lsi/dad/HttpServerVerticle.java
@@ -25,41 +25,41 @@ public class HttpServerVerticle extends AbstractVerticle {
 		router.route("/api/usuarios/*").handler(BodyHandler.create());
 		router.get("/api/usuarios").handler(this::obtenerUsuarios);
 		router.post("/api/usuarios/anadirUsuario").handler(this::anadirUsuario);
-		router.put("/api/usuarios/editarUsuario/:nif").handler(this::editarUsuario);
-		router.delete("/api/usuarios/:nif").handler(this::borrarUsuario);
+		router.put("/api/usuarios/editarUsuario/nif").handler(this::editarUsuario);
+		router.delete("/api/usuarios/borrarUsuario/nif").handler(this::borrarUsuario);
 
 		// PLACA
 		router.route("/api/placas/*").handler(BodyHandler.create());
 		router.get("/api/placas").handler(this::obtenerPlacas);
 		router.get("/api/placas/usuario").handler(this::obtenerPlacasUsuario);
 		router.post("/api/placas/anadirPlaca").handler(this::anadirPlaca);
-		router.put("/api/placas/editarPlaca/:id_placa").handler(this::editarPlaca);
-		router.delete("/api/placas/:id_placa").handler(this::borrarPlaca);
+		router.put("/api/placas/editarPlaca/oid_placa").handler(this::editarPlaca);
+		router.delete("/api/placas/borrarPlaca/oid_placa").handler(this::borrarPlaca);
 
 		// ALARMA
 		router.route("/api/alarmas/*").handler(BodyHandler.create());
 		router.get("/api/alarmas").handler(this::obtenerAlarmas);
-		router.get("/api/alarmas/:nif_fk").handler(this::obtenerAlarmasUsuario);
+		router.get("/api/alarmas/nif_fk").handler(this::obtenerAlarmasUsuario);
 		router.post("/api/alarmas/anadirAlarma").handler(this::anadirAlarma);
-		router.put("/api/alarmas/editarAlarma/:oid_alarma").handler(this::editarAlarma);
-		router.delete("/api/alarmas/:oid_alarma").handler(this::borrarAlarma);
+		router.put("/api/alarmas/editarAlarma/oid_alarma").handler(this::editarAlarma);
+		router.delete("/api/alarmas/oid_alarma").handler(this::borrarAlarma);
 
 		// LLAMADA
 		router.route("/api/llamadas/*").handler(BodyHandler.create());
 		router.get("/api/llamadas").handler(this::obtenerLlamadas);
-		router.get("/api/llamadas/enviadas/:remitente_nif_fk").handler(this::obtenerLlamadasEnviadasUsuario);
-		router.get("/api/llamadas/recibidas/:destinatario_nif_fk").handler(this::obtenerLlamadasRecibidasUsuario);
+		router.get("/api/llamadas/enviadas/remitente_nif_fk").handler(this::obtenerLlamadasEnviadasUsuario);
+		router.get("/api/llamadas/recibidas/destinatario_nif_fk").handler(this::obtenerLlamadasRecibidasUsuario);
 		router.post("/api/llamadas/anadirLlamada").handler(this::anadirLlamada);
-		router.put("/api/llamadas/editarLlamada/:oid_llamada").handler(this::editarLlamada);
+		router.put("/api/llamadas/editarLlamada/oid_llamada").handler(this::editarLlamada);
 		
 		//REGISTRO
 		router.route("/api/registros/*").handler(BodyHandler.create());
 		router.get("/api/registros/").handler(this::obtenerRegistros);
 		router.get("/api/registros/llamadas").handler(this::obtenerRegistrosLlamadas);
 		router.get("/api/registros/alarmas").handler(this::obtenerRegistrosAlarmas);
-		router.get("/api/registros/alarmas/:nif_fk").handler(this::obtenerRegistrosAlarmasUsuario);
-		router.get("/api/registros/llamadas/enviadas/:remitente_nif_fk").handler(this::obtenerRegistrosLlamadasEnviadas);
-		router.get("/api/registros/llamadas/recibidas/:destinatario_nif_fk").handler(this::obtenerRegistrosLlamadasRecibidas);
+		router.get("/api/registros/alarmas/nif_fk").handler(this::obtenerRegistrosAlarmasUsuario);
+		router.get("/api/registros/llamadas/enviadas/remitente_nif_fk").handler(this::obtenerRegistrosLlamadasEnviadas);
+		router.get("/api/registros/llamadas/recibidas/destinatario_nif_fk").handler(this::obtenerRegistrosLlamadasRecibidas);
 		
 			//Server HTTP
 		httpServer = vertx.createHttpServer();
@@ -97,8 +97,8 @@ public class HttpServerVerticle extends AbstractVerticle {
 	}
 
 	private void borrarUsuario(RoutingContext routingContext) {
-		String nif = routingContext.request().getParam("nif");
-		vertx.eventBus().request("borrarUsuario", nif, reply -> {
+		JsonObject json = routingContext.getBodyAsJson();
+		vertx.eventBus().request("borrarUsuario", json.getString("nif"), reply -> {
 			if (reply.succeeded()) {
 				System.out.println(reply.result().body());
 				routingContext.response().setStatusCode(200).putHeader("content-type", "application/json")
@@ -125,12 +125,10 @@ public class HttpServerVerticle extends AbstractVerticle {
 	}
 
 	private void editarUsuario(RoutingContext routingContext) {
-		String nif = routingContext.request().getParam("nif");
 		//json de los datos a modificar del usuario
 		JsonObject json = routingContext.getBodyAsJson();
 		//Añadir el nif que identifica al usuario para su modificacion
-		json.put("nif", nif);
-		vertx.eventBus().request("editarUsuario", json.toString(), reply -> {
+		vertx.eventBus().request("editarUsuario", json.getString("nif"), reply -> {
 			if (reply.succeeded()) {
 				System.out.println(reply.result().body());
 				routingContext.response().setStatusCode(200).putHeader("content-type", "application/json")
@@ -165,7 +163,6 @@ public class HttpServerVerticle extends AbstractVerticle {
 	}
 	
 	private void obtenerPlacasUsuario(RoutingContext routingContext) {
-		//String nif_fk = routingContext.request().getParam("nif_fk");
 		JsonObject json = routingContext.getBodyAsJson();
 		json.put("nif_fk", json.getString("nif_fk"));
 		vertx.eventBus().request("obtenerPlacasUsuario", json.toString(), reply -> {
@@ -181,8 +178,8 @@ public class HttpServerVerticle extends AbstractVerticle {
 	}
 
 	private void borrarPlaca(RoutingContext routingContext) {
-		String oid_placa = routingContext.request().getParam("oid_placa");
-		vertx.eventBus().request("borrarPlaca", oid_placa, reply -> {
+		JsonObject json = routingContext.getBodyAsJson();
+		vertx.eventBus().request("borrarPlaca", json.getString("oid_placa"), reply -> {
 			if (reply.succeeded()) {
 				System.out.println(reply.result().body());
 				routingContext.response().setStatusCode(200).putHeader("content-type", "application/json")
@@ -208,10 +205,8 @@ public class HttpServerVerticle extends AbstractVerticle {
 	}
 
 	private void editarPlaca(RoutingContext routingContext) {
-		String oid_placa = routingContext.request().getParam("id_placa");
-		System.out.println(oid_placa);
 		JsonObject json = routingContext.getBodyAsJson();
-		json.put("oid_placa", oid_placa);
+		json.put("oid_placa", json.getString("oid_placa"));
 		vertx.eventBus().request("editarPlaca", json.toString(), reply -> {
 			if (reply.succeeded()) {
 				System.out.println(reply.result().body());
@@ -247,9 +242,8 @@ public class HttpServerVerticle extends AbstractVerticle {
 	}
 	
 	private void obtenerAlarmasUsuario(RoutingContext routingContext) {
-		String nif_fk = routingContext.request().getParam("nif_fk");
 		JsonObject json = routingContext.getBodyAsJson();
-		json.put("nif_fk", nif_fk);
+		json.put("nif_fk", json.getString("nif_fk"));
 		vertx.eventBus().request("obtenerAlarmasUsuario", json.toString(), reply -> {
 			if (reply.succeeded()) {
 				System.out.println(reply.result().body());
@@ -263,8 +257,8 @@ public class HttpServerVerticle extends AbstractVerticle {
 	}
 
 	private void borrarAlarma(RoutingContext routingContext) {
-		String oid_alarma = routingContext.request().getParam("oid_alarma");
-		vertx.eventBus().request("borrarAlarma", oid_alarma, reply -> {
+		JsonObject json = routingContext.getBodyAsJson();
+		vertx.eventBus().request("borrarAlarma", json.getString("oid_alarma"), reply -> {
 			if (reply.succeeded()) {
 				System.out.println(reply.result().body());
 				routingContext.response().setStatusCode(200).putHeader("content-type", "application/json")
@@ -290,10 +284,8 @@ public class HttpServerVerticle extends AbstractVerticle {
 	}
 
 	private void editarAlarma(RoutingContext routingContext) {
-		String oid_alarma = routingContext.request().getParam("oid_alarma");
-		System.out.println(oid_alarma);
 		JsonObject json = routingContext.getBodyAsJson();
-		json.put("oid_alarma", oid_alarma);
+		json.put("oid_alarma", json.getString("oid_alarma"));
 		vertx.eventBus().request("editarAlarma", json.toString(), reply -> {
 			if (reply.succeeded()) {
 				System.out.println(reply.result().body());
@@ -329,9 +321,8 @@ public class HttpServerVerticle extends AbstractVerticle {
 	}
 	
 	private void obtenerLlamadasRecibidasUsuario(RoutingContext routingContext) {
-		String destinatario_nif_fk = routingContext.request().getParam("destinatario_nif_fk");
 		JsonObject json = routingContext.getBodyAsJson();
-		json.put("destinatario_nif_fk", destinatario_nif_fk);
+		json.put("destinatario_nif_fk", json.getString("destinatario_nif_fk"));
 		vertx.eventBus().request("obtenerLlamadasRecibidasUsuario", json.toString(), reply -> {
 			if (reply.succeeded()) {
 				System.out.println(reply.result().body());
@@ -345,9 +336,8 @@ public class HttpServerVerticle extends AbstractVerticle {
 	}
 	
 	private void obtenerLlamadasEnviadasUsuario(RoutingContext routingContext) {
-		String remitente_nif_fk = routingContext.request().getParam("remitente_nif_fk");
 		JsonObject json = routingContext.getBodyAsJson();
-		json.put("remitente_nif_fk", remitente_nif_fk);
+		json.put("remitente_nif_fk", json.getString("remitente_nif_fk"));
 		vertx.eventBus().request("obtenerLlamadasEnviadasUsuario", json.toString(), reply -> {
 			if (reply.succeeded()) {
 				System.out.println(reply.result().body());
@@ -374,9 +364,8 @@ public class HttpServerVerticle extends AbstractVerticle {
 	}
 
 	private void editarLlamada(RoutingContext routingContext) {
-		String oid_llamada = routingContext.request().getParam("oid_llamada");
 		JsonObject json = routingContext.getBodyAsJson();
-		json.put("oid_llamada", oid_llamada);
+		json.put("oid_llamada", json.getString("oid_llamada"));
 		vertx.eventBus().request("editarLlamada", json.toString(), reply -> {
 			if (reply.succeeded()) {
 				System.out.println(reply.result().body());
@@ -438,9 +427,8 @@ public class HttpServerVerticle extends AbstractVerticle {
 	}
 	
 	private void obtenerRegistrosAlarmasUsuario(RoutingContext routingContext) {
-		String nif_fk = routingContext.request().getParam("nif_fk");
 		JsonObject json = routingContext.getBodyAsJson();
-		json.put("nif_fk", nif_fk);
+		json.put("nif_fk", json.getString("nif_fk"));
 		vertx.eventBus().request("obtenerRegistrosAlarmasUsuario", json.toString(), reply -> {
 			if (reply.succeeded()) {
 				System.out.println(reply.result().body());
@@ -454,9 +442,8 @@ public class HttpServerVerticle extends AbstractVerticle {
 	}
 	
 	private void obtenerRegistrosLlamadasEnviadas(RoutingContext routingContext) {
-		String remitente_nif_fk = routingContext.request().getParam("remitente_nif_fk");
 		JsonObject json = routingContext.getBodyAsJson();
-		json.put("remitente_nif_fk", remitente_nif_fk);
+		json.put("remitente_nif_fk", json.getString("remitente_nif_fk"));
 		vertx.eventBus().request("obtenerRegistrosLlamadasEnviadas", json.toString(), reply -> {
 			if (reply.succeeded()) {
 				System.out.println(reply.result().body());
@@ -470,9 +457,8 @@ public class HttpServerVerticle extends AbstractVerticle {
 	}
 	
 	private void obtenerRegistrosLlamadasRecibidas(RoutingContext routingContext) {
-		String destinatario_nif_fk = routingContext.request().getParam("destinatario_nif_fk");
 		JsonObject json = routingContext.getBodyAsJson();
-		json.put("destinatario_nif_fk", destinatario_nif_fk);
+		json.put("destinatario_nif_fk", json.getString("destinatario_nif_fk"));
 		vertx.eventBus().request("obtenerRegistrosLlamadasRecibidas", json.toString(), reply -> {
 			if (reply.succeeded()) {
 				System.out.println(reply.result().body());
