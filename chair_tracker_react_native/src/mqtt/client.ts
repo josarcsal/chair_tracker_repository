c25c8092e9b9e5f9d72f6f9047dce0121b700416
josarcsal <josearciniega30@gmail.com>
2022-05-06 14:10:04 +0200

    fix: control errors in api and refactor trigger

--- chair_tracker_react_native/src/mqtt/client.ts
+++ chair_tracker_react_native/src/mqtt/client.ts
@@ -1,39 +1,39 @@
-import { MQTT_ENDPOINT } from '@env';
-import MQTT from 'sp-react-native-mqtt';
+// import { MQTT_ENDPOINT } from '@env';
+// import MQTT from 'sp-react-native-mqtt';
 
-export const setupMqtt = () => {
-  console.log(MQTT_ENDPOINT);
-  const MqttClient = MQTT.createClient({
-    uri: 'mqtt://' + MQTT_ENDPOINT,
-    clientId: '1001',
-  })
-    .then(function (client) {
-      client.on('closed', function () {
-        console.log('mqtt.event.closed');
-      });
+// export const setupMqtt = () => {
+//   console.log(MQTT_ENDPOINT);
+//   const MqttClient = MQTT.createClient({
+//     uri: 'mqtt://' + MQTT_ENDPOINT,
+//     clientId: '1001',
+//   })
+//     .then(function (client) {
+//       client.on('closed', function () {
+//         console.log('mqtt.event.closed');
+//       });
 
-      client.on('error', function (msg) {
-        console.log('mqtt.event.error', msg);
-      });
+//       client.on('error', function (msg) {
+//         console.log('mqtt.event.error', msg);
+//       });
 
-      client.on('message', function (msg) {
-        console.log('mqtt.event.message', msg);
-      });
+//       client.on('message', function (msg) {
+//         console.log('mqtt.event.message', msg);
+//       });
 
-      client.on('connect', function () {
-        console.log('connected');
-        client.subscribe('/data', 0);
-        client.publish('/data', 'test', 0, false);
-      });
+//       client.on('connect', function () {
+//         console.log('connected');
+//         client.subscribe('/data', 0);
+//         client.publish('/data', 'test', 0, false);
+//       });
 
-      client.connect();
+//       client.connect();
 
-      client.subscribe('/mac123/alarmas', 0);
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
-  return MqttClient;
-};
+//       client.subscribe('/mac123/alarmas', 0);
+//     })
+//     .catch(function (err) {
+//       console.log(err);
+//     });
+//   return MqttClient;
+// };
 
-export default setupMqtt;
+// export default setupMqtt;
